7f21f18e3603c0ba301a708a473a3fa1065c690b
Rudya93 <rudyaa93@gmail.com>
2018-05-08 19:38:48 +0300

    2

---
+++ var.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+export DB_USER=userdem
+export DB_PASS=dem123 
+export DB_HOST=192.168.0.11 
+export DB_PORT=3306 
+export DB_NAME=data
+export db_user=userdem
+export db_pass=dem123 
+export db_host=192.168.0.11 
+export db_port=3306 
+export db_name=data
+
+vagrant provision db
